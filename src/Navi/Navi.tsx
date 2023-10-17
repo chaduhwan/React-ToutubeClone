96c44616227786a8239d68e51f7aff86f7770ee6
chaduhwan <enghks2973@naver.com>
2023-10-17 20:49:45 +0900

    build

--- src/Navi/Navi.tsx
+++ src/Navi/Navi.tsx
@@ -56,18 +56,18 @@ export default function Navi() {
 
             <div className='linkcon'>
                 <div>
-                    <a>정보</a> <a>보도자료</a> <a>저작권</a> <a>문의하기</a> <a>크리에이터</a> <a>광고</a> <a>개발자</a>
+                    <p>정보</p> <p>보도자료</p> <p>저작권</p> <p>문의하기</p> <p>크리에이터</p> <p>광고</p> <p>개발자</p>
                 </div>
 
                 <div>
-                <a>약관</a> <a>개인정보처리방침</a> 
+                <p>약관</p> <p>개인정보처리방침</p> 
                     <div>
-                        <a>정책 및 안전</a>
-                        <a>YouTube 작동의 원리</a>
-                        <a>새로운 기능 테스트하기</a>
+                        <p>정책 및 안전</p>
+                        <p>YouTube 작동의 원리</p>
+                        <p>새로운 기능 테스트하기</p>
                     </div>
                     <div className='bottomdiv'>
-                    © 2023 Google LLC, Sundar Pichai, 1600 Amphitheatre Parkway, Mountain View CA 94043, USA, 0807-882-594 (무료), yt-support-solutions-kr@google.com, 호스팅: Google LLC, <p>사업자정보, 불법촬영물 신고</p>
+                    © 2023 Google LLC, Sundar Pichai, 1600 Amphitheatre Parkway, Mountain View CA 94043, USA, 0807-882-594 (무료), yt-support-solutions-kr@google.com, 호스팅: Google LLC, <p className='p'>사업자정보, 불법촬영물 신고</p>
 크리에이터들이 유튜브 상에 게시, 태그 또는 추천한 상품들은 판매자들의 약관에 따라 판매됩니다. 유튜브는 이러한 제품들을 판매하지 않으며, 그에 대한 책임을 지지 않습니다.
                     </div>
                 </div>
